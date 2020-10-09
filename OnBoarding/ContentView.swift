fb86e657897789b5036ba67aa06575e2d228f260
kasirajan22 <kasi.srm@gmail.com>
2020-10-09 07:25:47 +0530

    Initial Commit

---
+++ OnBoarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnBoarding
+//
+//  Created by Magizhan on 09/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by Magizhan on 09/10/20.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
